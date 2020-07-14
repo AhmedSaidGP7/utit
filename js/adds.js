031023091e413d9a145bb08e44ec8d416ebdef96
AhmedSaidGP7 <ahmed.saidgp7@gmail.com>
2020-07-14 17:00:25 +0200

    first version

---
+++ js/adds.js
@@ -0,0 +1,22 @@
+/*
+$(window).load(function(){
+	
+	$(".overload").fadeOut(1700);
+	
+	
+	
+			
+		
+});
+
+
+var myVar;
+
+function myFunction() {
+    myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById(".overload").style.display = "none";
+  
+}
